f6493aace620cf37b6a64c207303c59dcd72c932
mmarifernandes <marinafernandess103@gmail.com>
2022-06-13 21:27:17 -0300

    up

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ app.post('/detalhar', async (req, res) => {
     return res.send("ooops, id nao encontrado");
 })
 
-app.get('/home', async (req, res) => {
+app.get('/', async (req, res) => {
     const resultado = await AllShows();
 
 
@@ -60,30 +60,30 @@ app.get('/showdetail/:id', async (req, res) => {
     return res.send("ooops, id nao encontrado");
 })
 
-app.get('/', async (req, res) => {
+// app.get('/', async (req, res) => {
 
-    // https://swapi.dev/api/films/NUMERO
+//     // https://swapi.dev/api/films/NUMERO
 
-    // const numero = Math.floor(1 + Math.random() * 6);
-    const urlBusca = `https://api.tvmaze.com/search/shows?q=${req.query.show}`; 
+//     // const numero = Math.floor(1 + Math.random() * 6);
+//     const urlBusca = `https://api.tvmaze.com/search/shows?q=${req.query.show}`; 
 
-    try {
-        const responseApi = await axios.get(urlBusca);
+//     try {
+//         const responseApi = await axios.get(urlBusca);
 
-        res.send({
-            // numero,
-            urlBusca,
-            statusBusca: responseApi.status,
-            dataBusca: responseApi.data
-        });
-    } catch (error) {
-        console.log({error})
-        if (error.response.status == 404)
-            res.send('NAO FOI POSSIVEL LOCALIZAR O RECURSO NA API');
-        else
-            res.send('ERRO DESCONHECIDO...');
-    }
+//         res.send({
+//             // numero,
+//             urlBusca,
+//             statusBusca: responseApi.status,
+//             dataBusca: responseApi.data
+//         });
+//     } catch (error) {
+//         console.log({error})
+//         if (error.response.status == 404)
+//             res.send('NAO FOI POSSIVEL LOCALIZAR O RECURSO NA API');
+//         else
+//             res.send('ERRO DESCONHECIDO...');
+//     }
     
-});
+// });
 
 app.listen(3000, () => console.log("Escutando na porta 3000"));
